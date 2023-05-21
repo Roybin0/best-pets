03bfe86e3d6e5a50431797958862d3299de9957e
Robyn Smith <robyn.roybin@gmail.com>
2023-05-21 16:56:59 +0000

    Added routing for home, sign in and sign up pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,26 +4,42 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <Navbar className={styles.NavBar} expand="lg" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} alt="logo" className={styles.Img} />
-                </Navbar.Brand>
+                <NavLink to='/'>
+                    <Navbar.Brand href="#home">
+                        <img src={logo} alt="logo" className={styles.Img} />
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto text-left">
-                        <Nav.Link><i className="fas fa-house"></i> Home</Nav.Link>
-                        <Nav.Link><i className="fas fa-user"></i> Owners</Nav.Link>
-                        <Nav.Link><i className="fas fa-paw"></i> Pets</Nav.Link>
-                        <Nav.Link><i className="fas fa-image"></i> Pics</Nav.Link>
-                        <Nav.Link><i className="fas fa-book"></i> Tales</Nav.Link> 
-                        <Nav.Link>Profile</Nav.Link>
-                        <Nav.Link><i className="fas fa-sign-out-alt"></i> Sign Out</Nav.Link>
-                        <Nav.Link><i className="fas fa-sign-in-alt"></i> Sign In</Nav.Link>
-                        <Nav.Link><i className="fas fa-plus"></i> Sign Up</Nav.Link>
+                        <NavLink
+                            exact
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                            to='/'
+                        >
+                            <i className="fas fa-house"></i> Home
+                        </NavLink>
+                        <NavLink
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                            to='/'
+                        >
+                            <i className="fas fa-user"></i> Owners
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/'><i className="fas fa-paw"></i> Pets</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/'><i className="fas fa-image"></i> Pics</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/'><i className="fas fa-book"></i> Tales</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/'>Profile</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/'><i className="fas fa-sign-out-alt"></i> Sign Out</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signin'><i className="fas fa-sign-in-alt"></i> Sign In</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signup'><i className="fas fa-plus"></i> Sign Up</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
