065b485d80a9af25c735cefa0421127c548c7a10
Robyn Smith <robyn.roybin@gmail.com>
2023-05-24 21:26:38 +0000

    Fixed bug with form.select and adding basic styling

--- src/pages/pets/PetCreateForm.js
+++ src/pages/pets/PetCreateForm.js
@@ -31,40 +31,40 @@ function PetCreateForm() {
 
     const handleChange = (event) => {
         setPetData({
-          ...petData,
-          [event.target.name]: event.target.value,
-        });
-      };
-    
-      const handleChangeImage = (event) => {
-        if (event.target.files.length) {
-          URL.revokeObjectURL(image)
-          setPetData({
             ...petData,
-            image: URL.createObjectURL(event.target.files[0])
-          });
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleChangeImage = (event) => {
+        if (event.target.files.length) {
+            URL.revokeObjectURL(image)
+            setPetData({
+                ...petData,
+                image: URL.createObjectURL(event.target.files[0])
+            });
         }
-      };
-    
-      const handleSubmit = async (event) => {
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         const formData = new FormData();
-    
+
         formData.append("name", name)
         formData.append("pet_type", pet_type)
         formData.append("image", imageInput.current.files[0])
         formData.append("about", about)
-    
+
         try {
-          const { data } = await axiosReq.post("/pets/", formData);
-          history.push(`/pets/${data.id}`);
+            const { data } = await axiosReq.post("/pets/", formData);
+            history.push(`/pets/${data.id}`);
         } catch (err) {
-          console.log(err)
-          if (err.response?.status !== 401) {
-            setErrors(err.response?.data);
-          }
+            console.log(err)
+            if (err.response?.status !== 401) {
+                setErrors(err.response?.data);
+            }
         }
-      };
+    };
 
     const textFields = (
         <div className="text-center">
@@ -85,11 +85,12 @@ function PetCreateForm() {
 
             <Form.Group>
                 <Form.Label>Pet Type</Form.Label>
-                <Form.Select 
-                    name="pet_type" 
+                <Form.Control as="select"
+                    name="pet_type"
                     value={pet_type}
                     onChange={handleChange}
                 >
+                    <option>Choose from the list below</option>
                     <option value="Dog">Dog</option>
                     <option value="Cat">Cat</option>
                     <option value="Hamster">Hamster</option>
@@ -97,9 +98,25 @@ function PetCreateForm() {
                     <option value="Reptile">Reptile</option>
                     <option value="Rabbit">Rabbit</option>
                     <option value="Other">Other</option>
-                </Form.Select>
+                </Form.Control>
             </Form.Group>
             {errors?.pet_type?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            <Form.Group>
+                <Form.Label>About</Form.Label>
+                <Form.Control
+                    type="textarea"
+                    name="about"
+                    rows={3}
+                    value={about}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors?.about?.map((message, idx) => (
                 <Alert variant="warning" key={idx}>
                     {message}
                 </Alert>
@@ -119,57 +136,57 @@ function PetCreateForm() {
 
     return (
         <>
-        <Form onSubmit={handleSubmit}>
-            <Row>
-                <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-                    <Container
-                        className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-                    >
-                        <Form.Group className="text-center">
-                            {image ? (
-                                <>
-                                    <figure>
-                                        <Image className={appStyles.Image} src={image} rounded />
-                                    </figure>
-                                    <div>
-                                        <Form.Label
-                                            className={`${btnStyles.Button} btn`}
-                                            htmlFor="image-upload"
-                                        >
-                                            Change the image
-                                        </Form.Label>
-                                    </div>
-                                </>
-                            ) : (
-                                <Form.Label
-                                    className="d-flex justify-content-center"
-                                    htmlFor="image-upload"
-                                >
-                                    <Asset src={upload} message="Click or tap to upload an image" />
-                                </Form.Label>
+            <Form onSubmit={handleSubmit}>
+                <Row>
+                    <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+                        <Container
+                            className={`${styles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+                        >
+                            <Form.Group className="text-center">
+                                {image ? (
+                                    <>
+                                        <figure>
+                                            <Image className={appStyles.Image} src={image} rounded />
+                                        </figure>
+                                        <div>
+                                            <Form.Label
+                                                className={`${btnStyles.Button} btn`}
+                                                htmlFor="image-upload"
+                                            >
+                                                Change the image
+                                            </Form.Label>
+                                        </div>
+                                    </>
+                                ) : (
+                                    <Form.Label
+                                        className="d-flex justify-content-center"
+                                        htmlFor="image-upload"
+                                    >
+                                        <Asset src={upload} message="Click or tap to upload an image" />
+                                    </Form.Label>
 
-                            )}
+                                )}
 
-                            <Form.File
-                                id="image-upload"
-                                accept="image/*"
-                                onChange={handleChangeImage}
-                                ref={imageInput}
-                            />
-                        </Form.Group>
-                        {errors?.image?.map((message, idx) => (
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                        <div className="d-md-none">{textFields}</div>
-                    </Container>
-                </Col>
-                <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-                    <Container className={appStyles.Content}>{textFields}</Container>
-                </Col>
-            </Row>
-        </Form>
+                                <Form.File
+                                    id="image-upload"
+                                    accept="image/*"
+                                    onChange={handleChangeImage}
+                                    ref={imageInput}
+                                />
+                            </Form.Group>
+                            {errors?.image?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>
+                                    {message}
+                                </Alert>
+                            ))}
+                            <div className="d-md-none">{textFields}</div>
+                        </Container>
+                    </Col>
+                    <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+                        <Container className={styles.Content}>{textFields}</Container>
+                    </Col>
+                </Row>
+            </Form>
         </>
     );
 }
